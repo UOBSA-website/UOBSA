a9f543ff2cf7670c6784a3f4b3cf671f53b09d4a
Mehedi Hossain <72048371+MehediFH@users.noreply.github.com>
2025-01-04 11:35:01 -0500

    Create react app

---
+++ uobsa/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const App = () => {
+    return (
+      <div>
+      <h1>Hello UOBSA!</h1>
+      <p>This is a test to verify if rendering works.</p>
+    </div>
+    );
+};
+
+export default App;
